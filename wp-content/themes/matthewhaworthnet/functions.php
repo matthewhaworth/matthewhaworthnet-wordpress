6084299994e5c3e4c080b39a8a6448b98352f120
matthew haworth <matthew@codepool.co.uk>
2013-08-22 23:30:33 +0100

    corrected category html

--- wp-content/themes/matthewhaworthnet/functions.php
+++ wp-content/themes/matthewhaworthnet/functions.php
@@ -13,3 +13,10 @@ function the_paginator() {
             'total' => $wp_query->max_num_pages
     ) );
 }
+
+function remove_category_rel($output)
+{
+    $output = str_replace(' rel="category tag"', '', $output);
+    return $output;
+}
+add_filter('the_category', 'remove_category_rel');
