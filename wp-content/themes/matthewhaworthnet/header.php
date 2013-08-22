b54064ead66a4a372691c6881577699f65e73571
matthew haworth <matthew@codepool.co.uk>
2013-08-22 20:57:59 +0100

    added sitemap and updated keywords

--- wp-content/themes/matthewhaworthnet/header.php
+++ wp-content/themes/matthewhaworthnet/header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>matthew haworth | <?php the_title(); ?></title>
+    <title>manchester based magento web developer | <?php the_title(); ?> | matthew haworth</title>
     <link rel="shortcut icon" href="/favicon.ico?v=2" />
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="Manchester based Magento web developer blog about every day experiences"/>
+    <meta name="keywords" content="magento, manchester, blog, php, zend, developer, magento developer" />
+    <meta name="author" content="matthew haworth" />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/reset.css" />
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
@@ -17,7 +20,7 @@
             <h1><a href='<?php echo get_site_url(); ?>'>matthew haworth</a></h1>
         </div>
         <div class="bio">
-            <h2>web developer</h2>
+            <h2>magento web developer</h2>
         </div>
         <div class="clear"></div>
         <hr class="thick"/>
